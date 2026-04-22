974430b137177e75a8d18b73d539b56bfba7a221
Deekshith333 <deekshithvanaparthi@gmail.com>
2026-04-22 21:41:13 +0530

    Python basics

---
+++ Python/PythonCoding/basic_programs/forrange.py
@@ -0,0 +1,14 @@
+
+# #natural numbers
+# num=int(input('Enter a number : '))
+#
+# sum=0
+# for i in range(1,num+1):
+#     sum=sum+i**2
+# print('sum of sqsof nat numbers : ',sum)
+
+#asterisk
+num=int(input('Enter how many * u want : '))
+
+for _ in range(1,num+1):
+    print('*','&',sep='$$',end='---')
---
+++ Python/PythonCoding/basic_programs/whileloop.py
@@ -0,0 +1,10 @@
+
+# natural numbers print
+
+# num = int(input('Enter a number : '))
+# value = 1
+# 
+# while value < num:
+#     print(value)
+#     value+ural numbers
+#Armstrong nuural numbers
